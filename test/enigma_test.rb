26f62f50d90de4b19e04445b2d3dce31e2469376
Matt Levy <milevy1@gmail.com>
2019-02-21 12:50:05 -0700

    Added test it can encrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,23 @@
 require './test/test_helper'
+require 'date'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
-  
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_encrypt
+    expected = { encryption: "keder ohulw",
+                 key: "02715",
+                 date: "040895" }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
 end
