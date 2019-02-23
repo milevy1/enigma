07df0b4b486505bb536625525ee7ee79a3e9cb83
Matt Levy <milevy1@gmail.com>
2019-02-23 08:47:24 -0700

    Added test key_converter

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,6 +12,17 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_key_converter_converts_key_to_individual_keys
+    key = "02715"
+    expected = { a_key: 02,
+                 b_key: 27,
+                 c_key: 71,
+                 d_key: 15
+               }
+
+    assert_equal expected, @enigma.key_converter(key)
+  end
+
   def test_it_can_encrypt_with_a_key_and_date
     skip
     expected = { encryption: "keder ohulw",
