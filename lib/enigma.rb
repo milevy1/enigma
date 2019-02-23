7ef1399afbef75f6a8147d498d5dbf35f3d9c548
Matt Levy <milevy1@gmail.com>
2019-02-23 08:50:18 -0700

    Added key_converter

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,4 +4,12 @@ class Enigma
 
   end
 
+  def key_converter(key)
+    key = key.to_s
+    { a_key: key[0..1].to_i,
+      b_key: key[1..2].to_i,
+      c_key: key[2..3].to_i,
+      d_key: key[3..4].to_i }
+  end
+
 end
