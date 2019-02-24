3a2ee95fb669303c30e238f7839b2203d3502719
Matt Levy <milevy1@gmail.com>
2019-02-24 10:29:22 -0700

    Added decryption command line runner file

---
+++ lib/decrypt.rb
@@ -0,0 +1,19 @@
+require './lib/enigma'
+
+encrypted_file_name = ARGV[0]
+decrypted_file_name = ARGV[1]
+key = ARGV[2]
+date = ARGV[3]
+
+encrypted_file = File.open(encrypted_file_name, "r")
+message = encrypted_file.read
+encrypted_file.close
+
+enigma = Enigma.new
+decryption = enigma.decrypt(message, key, date)
+
+writer_file = File.open(decrypted_file_name, "w")
+writer_file.write(decryption[:decryption])
+writer_file.close
+
+puts "Created '#{decrypted_file_name}' with the key #{decryption[:key]} and date #{decryption[:date]}"
