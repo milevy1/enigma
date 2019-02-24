1d0de7586f91ca85141d58b3e94594a53858b4de
Matt Levy <milevy1@gmail.com>
2019-02-24 16:37:07 -0700

    Initial commit

---
+++ test/command_line_interface_test.rb
@@ -0,0 +1,11 @@
+require './test/test_helper'
+require 'date'
+require './lib/enigma'
+require './lib/encryption'
+require './lib/command_line_interface'
+
+class CommandLineInterfaceTest < Minitest::Test
+  include Encryption
+  include CommandLineInterface
+
+end
