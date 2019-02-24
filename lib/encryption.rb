9d265da22a41e21222b7e8b73097c6ae8536e80b
Matt Levy <milevy1@gmail.com>
2019-02-24 09:40:54 -0700

    Refactored do-end to {}

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -4,10 +4,9 @@ module Encryption
     shifts = shift_converter(key_converter(key), date_converter(date))
     encrypted_message = []
     counter = 1
-    message.downcase.chars.each do |character|
+    message.downcase.chars.each { |character|
       encrypted_message << encrypt_character(character, find_shift(counter, shifts))
-      counter = increment_counter(counter)
-    end
+      counter = increment_counter(counter) }
     { encryption: encrypted_message.join, key: key, date: date }
   end
 
@@ -15,10 +14,9 @@ module Encryption
     shifts = shift_converter(key_converter(key), date_converter(date))
     decrypted_message = []
     counter = 1
-    message.downcase.chars.each do |character|
+    message.downcase.chars.each { |character|
       decrypted_message << decrypt_character(character, find_shift(counter, shifts))
-      counter = increment_counter(counter)
-    end
+      counter = increment_counter(counter) }
     { decryption: decrypted_message.join, key: key, date: date }
   end
 
