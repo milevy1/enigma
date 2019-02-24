fd5b47f7dd1291e665b97015c037875e300382a8
Matt Levy <milevy1@gmail.com>
2019-02-23 18:23:19 -0700

    Initial commit

---
+++ lib/encryption.rb
@@ -0,0 +1,3 @@
+module Encryption
+  
+end
---
+++ test/encryption_test.rb
@@ -0,0 +1,10 @@
+require './test/test_helper'
+require 'date'
+require './lib/enigma'
+require './lib/encryption'
+
+class EncryptionTest < Minitest::Test
+  include Encryption
+
+
+end
