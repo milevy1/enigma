cd2c99ef6c2f168d0525bab3ee0cff73cb913095
Matt Levy <milevy1@gmail.com>
2019-02-24 10:30:32 -0700

    Rearranged lines for readability

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,12 +3,11 @@ require './lib/enigma'
 message_file_name = ARGV[0]
 encryption_file_name = ARGV[1]
 
-enigma = Enigma.new
-
 message_file = File.open(message_file_name, "r")
 message = message_file.read.chop
 message_file.close
 
+enigma = Enigma.new
 encryption = enigma.encrypt(message)
 
 writer_file = File.open(encryption_file_name, "w")
