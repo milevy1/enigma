51c984e7a9ed737d4ea923c633ce89c11e82f56c
Matt Levy <milevy1@gmail.com>
2019-02-24 10:16:23 -0700

    Refactored helper variables message_file_name and encryption_file_name

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,18 @@
 require './lib/enigma'
 
+message_file_name = ARGV[0]
+encryption_file_name = ARGV[1]
+
 enigma = Enigma.new
 
-message_file = File.open(ARGV[0], "r")
+message_file = File.open(message_file_name, "r")
 message = message_file.read.chop
 message_file.close
 
 encryption = enigma.encrypt(message)
 
-writer_file = File.open(ARGV[1], "w")
+writer_file = File.open(encryption_file_name, "w")
 writer_file.write(encryption[:encryption])
 writer_file.close
 
-puts "Created '#{ARGV[1]}' with the key #{encryption[:key]} and date #{encryption[:date]}"
+puts "Created '#{encryption_file_name}' with the key #{encryption[:key]} and date #{encryption[:date]}"
