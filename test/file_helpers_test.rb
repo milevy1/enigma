3c2d39a2ba6710f3c4e667ebf62e0427b48ec86a
Matt Levy <milevy1@gmail.com>
2019-02-25 15:48:07 -0700

    Added test_read_file_text

--- test/file_helpers_test.rb
+++ test/file_helpers_test.rb
@@ -4,5 +4,11 @@ require './lib/file_helpers'
 class FileHelpersTest < Minitest::Test
   include FileHelpers
 
+  def test_read_file_text
+    expected = "Hello World!"
+    file_name = "./test/test_file_text.txt"
+
+    assert_equal expected, read_file_text(file_name)
+  end
 
 end
