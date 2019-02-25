988fa4313b0101de9001a15fedb437b650a4bfa9
Matt Levy <milevy1@gmail.com>
2019-02-24 18:01:57 -0700

    Added last_four_rotated

--- lib/crack.rb
+++ lib/crack.rb
@@ -1,7 +1,12 @@
 module Crack
 
   def last_four_characters(message)
-    message[-4..-1].downcase.chars    
+    message[-4..-1].downcase.chars
+  end
+
+  def last_four_rotated(message)
+    rotate_amount = (message.length - 4) % 4
+    last_four_characters(message).rotate(-rotate_amount)
   end
 
 end
