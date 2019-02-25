6b175d44551601bedc9c76a26212e6235352d15f
Matt Levy <milevy1@gmail.com>
2019-02-24 17:02:05 -0700

    Refactored decrypt to use FileHelpers

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -5,15 +5,9 @@ decrypted_file_name = ARGV[1]
 key = ARGV[2]
 date = ARGV[3]
 
-encrypted_file = File.open(encrypted_file_name, "r")
-message = encrypted_file.read.chomp
-encrypted_file.close
-
 enigma = Enigma.new
+message = enigma.read_file_text(encrypted_file_name)
 decryption = enigma.decrypt(message, key, date)
-
-writer_file = File.open(decrypted_file_name, "w")
-writer_file.write(decryption[:decryption])
-writer_file.close
+enigma.write_file_text(decrypted_file_name, decryption[:decryption])
 
 puts "Created '#{decrypted_file_name}' with the key #{decryption[:key]} and date #{decryption[:date]}"
