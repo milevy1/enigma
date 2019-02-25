404f62a3927502d1f72c43f961c377c02c918598
Matt Levy <milevy1@gmail.com>
2019-02-25 10:36:43 -0700

    Refactored test

--- test/crack_test.rb
+++ test/crack_test.rb
@@ -76,10 +76,10 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_generate_potential_keys
-    shift = 1
+    shift = 2
     offset = 1
-    
-    assert_equal [2, 29, 56, 83], @enigma.generate_potential_keys(shift, offset)
+
+    assert_equal [1, 28, 55, 82], @enigma.generate_potential_keys(shift, offset)
   end
 
   def test_crack_shifts
