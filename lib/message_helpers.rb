96aed6af94b4842e51ddab5922b4ba65a29d9afd
Matt Levy <milevy1@gmail.com>
2019-02-25 17:11:13 -0700

    Initial commit

---
+++ lib/message_helpers.rb
@@ -0,0 +1,3 @@
+module MessageHelpers
+
+end
---
+++ test/message_helpers_test.rb
@@ -0,0 +1,10 @@
+require './test/test_helper'
+require 'date'
+require './lib/enigma'
+require './lib/message_helpers'
+
+class MessageHelpersTest < Minitest::Test
+
+
+
+end
