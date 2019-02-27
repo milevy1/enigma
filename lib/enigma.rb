a32b201f0ee68a94ca35d40754ad20f6e7f45325
Matt Levy <milevy1@gmail.com>
2019-02-26 20:14:53 -0700

    Relocated encrypt/decrypt from Encryption module to Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,4 +9,25 @@ class Enigma
   include FileHelpers
   include CrackHelpers
   include MessageHelpers
+
+  def encrypt(message, key = random_key, date = todays_date)
+    shifts = shift_converter(key_converter(key), date_converter(date))
+    encrypted_message = []
+    counter = 1
+    message.downcase.chars.each { |character|
+      encrypted_message << encrypt_character(character, find_shift(counter, shifts))
+      counter = increment_counter(counter) }
+    { encryption: encrypted_message.join, key: key, date: date }
+  end
+
+  def decrypt(message, key, date = todays_date)
+    shifts = shift_converter(key_converter(key), date_converter(date))
+    decrypted_message = []
+    counter = 1
+    message.downcase.chars.each { |character|
+      decrypted_message << decrypt_character(character, find_shift(counter, shifts))
+      counter = increment_counter(counter) }
+    { decryption: decrypted_message.join, key: key, date: date }
+  end
+  
 end
