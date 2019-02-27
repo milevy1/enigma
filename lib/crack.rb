f9fe8bccbe35b2c509d656067c08b7c7c68e2d3e
Matt Levy <milevy1@gmail.com>
2019-02-26 20:32:42 -0700

    Added edge case for when incorrect date is entered and there is no possible key sequence.

--- lib/crack.rb
+++ lib/crack.rb
@@ -7,6 +7,10 @@ date = ARGV[2]
 enigma = Enigma.new
 message = enigma.read_file_text(encrypted_file_name)
 cracked = enigma.crack(message, date)
-enigma.write_file_text(cracked_file_name, cracked[:decryption])
 
-puts "Created '#{cracked_file_name}' with the key #{cracked[:key]} and date #{cracked[:date]}"
+if cracked == "There is no possible key sequence."
+  puts cracked + " Please check that you entered the correct date used for encryption."
+else
+  enigma.write_file_text(cracked_file_name, cracked[:decryption])
+  puts "Created '#{cracked_file_name}' with the key #{cracked[:key]} and date #{cracked[:date]}"
+end
